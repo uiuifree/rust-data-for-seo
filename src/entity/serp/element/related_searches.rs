b701b4fc3d617e20b0b268479daa563efc2df226
uiuifree <info@uiuifree.com>
2024-12-24 10:50:14 +0900

    serp

--- src/entity/serp/element/related_searches.rs
+++ src/entity/serp/element/related_searches.rs
@@ -9,6 +9,6 @@ pub struct SerpApiElementRelatedSearches {
     pub rank_absolute: Option<i32>,
     pub position: Option<String>,
     pub xpath: Option<String>,
-    pub items: Option<Value>,
+    pub items: Option<Vec<String>>,
     pub rectangle: Option<SerpApiRectangle>,
 }
